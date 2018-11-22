ff3f5906a01f88c4f86bfa785d92d346f8f9b142
cuongdq.payment <cuongdq.payment@gmail.com>
2018-11-22 18:46:06 +0700

    ignor

--- sqlite-main.js
+++ sqlite-main.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
+const dirDB = 'db';
+if (!fs.existsSync(dirDB)) {
+    fs.mkdirSync(dirDB);
+}
+
+
 var AppDAO = require('./sqlite-dynamic');
 
-var db = new AppDAO('./db-sqlite3.db');
+var db = new AppDAO('./dirDB/db-sqlite3.db');
